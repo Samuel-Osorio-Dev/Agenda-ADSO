2f8b2869ee16ac01226d02c8388764cf668911e9
Samuel Osorio <samuosop29@gmail.com>
2026-03-30 14:41:24 -0500

    Clase_12_Backend_Agenda_ADSO_API_JSON_Server

--- src/config.js
+++ src/config.js
@@ -1,12 +1,10 @@
 // src/config.js
 // Configuraciones generales de la Agenda ADSO
 
-// Dirección de la API — si cambia el puerto, solo se edita aquí
-export const API_BASE_URL = "http://localhost:3002/contactos";
+export const API_BASE_URL = "https://agenda-adso-api-4fdi.onrender.com/contactos";
 
-// Información del encabezado de la app
 export const APP_INFO = {
   ficha: "3229209",
   titulo: "Agenda ADSO v10",
-  subtitulo: "Gestión de contactos conectada a una API local con JSON Server, con validaciones y mejor experiencia de usuario.",
+  subtitulo: "Gestión de contactos conectada a una API remota en Render, con validaciones y mejor experiencia de usuario.",
 };
